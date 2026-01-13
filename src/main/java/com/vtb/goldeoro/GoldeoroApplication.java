756adb6cdf96ee554f48d11b8a297d236cabbe47
Elias <godtol69@gmail.com>
2026-01-13 17:30:58 -0300

    Initial commit

---
+++ src/main/java/com/vtb/goldeoro/GoldeoroApplication.java
@@ -0,0 +1,13 @@
+package com.vtb.goldeoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldeoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldeoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtb/goldeoro/GoldeoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtb.goldeoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldeoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
